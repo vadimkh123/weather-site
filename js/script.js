3b0adf6ddb1cb6877aba5425a69c6dfac16c9f52
Student <student@example.com>
2026-09-07 16:15:15 +0000

    Revert "Додано конвертацію температури у Фаренгейти"
    This reverts commit 39b1f0ad039b56e5c432df7baa11ffb6b620052c.

--- js/script.js
+++ js/script.js
@@ -3,13 +3,3 @@ function showWeather() {
 }
 
 showWeather();
-
-function celsiusToFahrenheit(celsius) {
-  // ПОМИЛКА: правильна формула — celsius * 9/5 + 32
-  return celsius * 2 + 30;
-}
-
-function celsiusToFahrenheit(celsius) {
-  // ПОМИЛКА: правильна формула — celsius * 9/5 + 32
-  return celsius * 2 + 30;
-}
